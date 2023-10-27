21107c6d87f9081c72269e36da8a2a891feea925
Arnab Barua <89764073+Arnab-000@users.noreply.github.com>
2023-10-28 01:14:28 +0600

    Initial Commit

---
+++ data/storage.js
@@ -0,0 +1,15 @@
+export const resource = [
+  {
+    question: "What is your name ?",
+    option: {
+      A: "Naruto Uzumaki",
+      B: "Sasuke Uchiha",
+      C: "Itachi",
+      D: "Kakashi",
+    },
+    answer: {
+      option: "C",
+      value: "Itachi",
+    },
+  },
+];
