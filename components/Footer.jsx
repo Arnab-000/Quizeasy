a3cc63e0e3cace4c712b347b446619569a77b855
Arnab Barua <89764073+Arnab-000@users.noreply.github.com>
2023-11-06 02:58:50 +0600

    Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,6 @@
-export default function Header() {
+export default function Footer() {
   return (
-    <div className=" w-[100%] fixed bottom-5 mx-auto text-[#c0c0c0] font-semibold text-sm text-center">
+    <div className=" w-[100%] h-fit absolute bottom-[1%] mx-auto text-[#c0c0c0] font-semibold text-sm text-center">
       &#169; Made by Arnab Barua using Tailwind Css, React Router, React.
     </div>
   );
