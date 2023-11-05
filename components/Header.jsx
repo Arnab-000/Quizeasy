1722500c14c43594d1813556d086095efc007eb8
Arnab Barua <89764073+Arnab-000@users.noreply.github.com>
2023-11-06 03:07:02 +0600

    Smaller Device Header

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,6 +2,10 @@ import am from "/assets/Quizeasy.png";
 
 export default function Header() {
   return (
-    <img src={am} alt="" className=" h-[100px] mt-[5%] mb-[-8%] mx-auto" />
+    <img
+      src={am}
+      alt=""
+      className=" h-[100px] mt-[5%]  mb-[-12%] md:mb-[-8%] mx-auto"
+    />
   );
 }
