32880a67cf3fe7fb0afcfe0f20c8b0a1c4756eae
Arnab Barua <89764073+Arnab-000@users.noreply.github.com>
2023-11-06 01:50:35 +0600

    Image Path

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <img
-      src="../assets/Quizeasy.png"
+      src="/assets/Quizeasy.png"
       alt=""
       className=" h-[100px] mt-[5%] mb-[-8%] mx-auto"
     />
